44414d44df7ae13f6ae1c19d3296958611b11b93
Nadil-Dulran <it23161788@my.sliit.lk>
2025-08-30 21:48:33 +0530

    Created basic AI_chat_bot
    Can chat with the bot in CLI

--- main.py
+++ main.py
@@ -16,5 +16,18 @@ model = OllamaLLM(model= "llama3.2")
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
-result = chain.invoke({"context": "", "question": "Hay how are you?"})
-print(result)
+def handle_conversation():
+    context = ""
+    print("Welcome to the My AI Chat Bot!, Type 'exit' to quit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            print("Exiting the Chat. Goodbye!")
+            break
+
+        result = chain.invoke({"context": context, "question": user_input})
+        print("AI BOT: ", result)
+        context += f"\nUser: {user_input}\nAI BOT: {result}"
+        
+if __name__ == "__main__":
+    handle_conversation()
